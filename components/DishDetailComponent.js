a14de01fc2393d31c3575de41318d30e1a4f1139
frisciliasultan <frisciliasultan@gmail.com>
2021-07-01 09:26:39 +0800

    Add gesture to dish detail

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -71,6 +71,11 @@ function RenderDish(props) {
         else return false;
     };
 
+    const recognizeComment = ({ moveX, moveY, dx, dy }) => {
+        if (dx > 200) return true;
+        else return false;
+    };
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
@@ -82,7 +87,7 @@ function RenderDish(props) {
         },
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
-            if (recognizeDrag(gestureState))
+            if (recognizeDrag(gestureState)) {
                 Alert.alert(
                     'Add Favorite',
                     'Are you sure you wish to add ' +
@@ -105,7 +110,9 @@ function RenderDish(props) {
                     ],
                     { cancelable: false }
                 );
-
+            } else if (recognizeComment(gestureState)) {
+                props.onWriteCommentPress();
+            }
             return true;
         }
     });
